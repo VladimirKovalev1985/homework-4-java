dfb64e74e867f9558a96bf6590638fd9fb71c104
Vladimir Kovalev <boban_envorter@mail.ru>
2021-10-13 19:16:27 +0300

    add files

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -2,6 +2,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
 
     @org.junit.jupiter.api.Test
     void shoudCalculateNotRegisteredAndUnderLimit() {
@@ -23,6 +55,7 @@ class BonusServiceTest {
         // производится проверка
         assertEquals(expected, actual);
     }
+
     @org.junit.jupiter.api.Test
     void shoudCalculateNotRegisteredAndOverLimit() {
         BonusService service = new BonusService();
@@ -30,7 +63,7 @@ class BonusServiceTest {
         // подготавливаем данные:
         long amount = 100_000_060;
         boolean notRegistered = false;
-        long expected = 5000;
+        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, notRegistered);
